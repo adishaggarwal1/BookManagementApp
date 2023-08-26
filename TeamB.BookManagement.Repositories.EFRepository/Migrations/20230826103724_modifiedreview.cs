a4300056ea216c2294c72e6f001ef54c4cf2e097
adish <adish.aggarwal@ecolab.com>
2023-08-26 16:21:07 +0530

    Updated Database

---
+++ TeamB.BookManagement.Repositories.EFRepository/Migrations/20230826103724_modifiedreview.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamB.BookManagement.Repositories.EFRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class modifiedreview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Book_Id",
+                table: "Reviews");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Book_Id",
+                table: "Reviews",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
